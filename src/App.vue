5ab527c1f58da91c158ab6fe954cb58d81cf670b
mayuxian <mayuxian@autohome.com.cn>
2021-03-31 10:28:41 +0800

    css的补充

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@
     <!--  测试
     使用flex布局，显示页面，展现列表。-->
     <!-- <element-center-sample></element-center-sample> -->
-    <canvas-sample></canvas-sample>
+    <!-- <canvas-sample></canvas-sample> -->
+    <MaskImage></MaskImage>
   </div>
 </template>
 
 <script>
 
 // import ElementCenterSample from './element-center/Sample';
-import CanvasSample from './canvas/Sample';
+// import CanvasSample from './canvas/Sample';
+import MaskImage from './animation/MaskImage.vue'
 export default {
   name: 'app',
   components: {
     // ElementCenterSample,
-    CanvasSample
+    // CanvasSample,
+    MaskImage
   }
 }
 </script>
---
+++ src/graph/TODO.md
@@ -0,0 +1,9 @@
+
+1. 用CSS绘制最常见的40种形状和图形
+https://www.webhek.com/post/40-css-shapes.html
+
+2. CSS 实现优惠券的技巧
+https://juejin.cn/post/6945023989555134494
+
+3. 简单说 CSS中的mask—好好利用mask-image
+https://segmentfault.com/a/1190000011838367
