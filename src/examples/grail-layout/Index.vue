5c314f9f737a1d131ca075726c217f1208e7e294
mayuxian <mayuxian@autohome.com.cn>
2021-12-20 15:53:09 +0800

    添加圣杯和双飞翼布局

---
+++ src/examples/grail-layout/Index.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1>圣杯布局</h1>
+  https://www.cnblogs.com/jiguiyan/p/11425276.html
+  <div id="bd">
+    <div id="middle">middle</div>
+    <div id="left">left</div>
+    <div id="right">right</div>
+  </div>
+</template>
+      
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+          
+<style lang='stylus' scoped>
+#bd {
+  padding: 0 200px 0 180px;
+  height: 100px;
+}
+
+#middle {
+  float: left;
+  width: 100%;
+  height: 500px;
+  background: blue;
+}
+
+#left {
+  float: left;
+  width: 180px;
+  height: 500px;
+  margin-left: -100%;
+  background: #0c9;
+  position: relative;
+  left: -180px;
+}
+
+#right {
+  float: left;
+  width: 200px;
+  height: 500px;
+  margin-left: -200px;
+  background: #0c9;
+  position: relative;
+  right: -200px;
+}
+</style>
---
+++ src/examples/shuangfeiyi-layout/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>双飞翼布局</h1>
+  https://www.cnblogs.com/jiguiyan/p/11425276.html
+  <div id="center">
+    <div id="inside">middle</div>
+  </div>
+  <div id="left">left</div>
+  <div id="right">right</div>
+</template>
+      
+<script>
+//https://www.cnblogs.com/jiguiyan/p/11425276.html
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+          
+<style lang='stylus' scoped>
+#center {
+  float: left;
+  width: 100%; /* 左栏上去到第一行 */
+  height: 100px;
+  background: blue;
+}
+
+#left {
+  float: left;
+  width: 180px;
+  height: 100px;
+  margin-left: -100%;
+  background: #0c9;
+}
+
+#right {
+  float: left;
+  width: 200px;
+  height: 100px;
+  margin-left: -200px;
+  background: #0c9;
+}
+
+/* 给内部div添加margin，把内容放到中间栏，其实整个背景还是100% */
+#inside {
+  margin: 0 200px 0 180px;
+  height: 100px;
+}
+</style>
